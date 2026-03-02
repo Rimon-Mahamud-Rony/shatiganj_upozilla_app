9c19331cdac7d3cd521763902acf0ce546d00d44
Rimon Mahamud Rony <rimonronycste11@gmail.com>
2026-03-02 12:21:33 +0600

    ci: project intialized

---
+++ components/shared/DrawerScrollableContet.tsx
@@ -0,0 +1,60 @@
+import { Button } from "@/components/ui/button";
+import { FiAlignJustify } from "react-icons/fi";
+import { AiOutlineClose } from "react-icons/ai";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
+export function DrawerScrollableContet() {
+  return (
+    <Drawer direction="right">
+      <DrawerTrigger asChild className="border border-indigo-500 rounded hover:bg-blue-500 hover:border-blue-500 hover:text-white hover:px-4  cursor-pointer">
+        <Button variant="outline">
+          <FiAlignJustify />
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <div className="flex flex-row justify-between">
+            <DrawerTitle>Move Goal</DrawerTitle>
+            <DrawerClose asChild>
+              <Button variant="outline" className="border border-red-500 hover:bg-red-500 hover:text-white cursor-pointer">
+                <AiOutlineClose />
+              </Button>
+            </DrawerClose>
+          </div>
+          <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+        </DrawerHeader>
+        <div className="no-scrollbar overflow-y-auto px-4">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <p
+              key={index}
+              className="style-lyra:mb-2 style-lyra:leading-relaxed mb-4 leading-normal"
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          ))}
+        </div>
+        <DrawerFooter>
+          <Button>Submit</Button>
+          {/* <DrawerClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose> */}
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+}
---
+++ components/shared/navbar.tsx
@@ -0,0 +1,86 @@
+import { DrawerScrollableContet } from "./DrawerScrollableContet";
+import { Noto_Serif_Bengali } from "next/font/google";
+import localFont from "next/font/local";
+import { IoHome } from "react-icons/io5";
+import { ImAddressBook } from "react-icons/im";
+import { IoMdNotifications } from "react-icons/io";
+import { RiLoginBoxFill } from "react-icons/ri";
+
+
+  const notoSerifBengali = Noto_Serif_Bengali({
+    variable: "--font-bengali",
+    subsets: ["bengali"],
+    weight: ["400", "500", "600", "700"],
+  });
+
+const solaimanLipi = localFont({
+  src: "../../source/font/SolaimanLipi.ttf",
+  weight: "400",
+  style: "normal",
+});
+
+export function Navbar() {
+
+
+  
+    return (
+      <header className="text-gray-600 body-font bg-white/40 backdrop-blur-md">
+        <div className="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center shadow-sm blur-in-md">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-2 md:mb-0">
+            <span
+              className={`ml-3 text-3xl font-semibold text-green-700 ${solaimanLipi.className}`}
+            >
+              জনসেবায় শান্তিগঞ্জ
+            </span>
+          </a>
+          <nav
+            className={`md:ml-auto flex flex-wrap items-center text-sm justify-center ${solaimanLipi.className}`}
+          >
+            <a
+              href="http://localhost:3000/"
+              className="mr-5 text-gray-900 cursor-pointer hover:text-green-500"
+            >
+              <div className="flex flex-row gap-x-1 text-xl">
+                <IoHome className="text-base mt-1" />
+                <p className="text-xl font-light">হোম</p>
+              </div>
+            </a>
+
+            <a
+              href="http://localhost:3000/"
+              className="mr-5 text-gray-900 cursor-pointer hover:text-green-500"
+            >
+              <div className="flex flex-row gap-x-1 text-xl">
+                <ImAddressBook className="text-base mt-1" />
+                <p className="text-xl font-light">যোগাযোগ</p>
+              </div>
+            </a>
+
+            <a
+              href="http://localhost:3000/"
+              className="mr-5 text-gray-900 cursor-pointer hover:text-green-500"
+            >
+              <div className="flex flex-row gap-x-1 text-xl">
+                <IoMdNotifications className="text-base mt-1" />
+                <p className="text-xl font-light">নোটিফিকেশন</p>
+              </div>
+            </a>
+
+            <a
+              href="http://localhost:3000/"
+              className="mr-5 text-gray-900 cursor-pointer hover:text-green-500"
+            >
+              <div className="flex flex-row gap-x-1 text-xl">
+                <RiLoginBoxFill className="text-base mt-1" />
+                <p className="text-xl font-light">ইউনিয়ন লগইন</p>
+              </div>
+            </a>
+
+            <button className="inline-flex items-center text-base">
+              <DrawerScrollableContet />
+            </button>
+          </nav>
+        </div>
+      </header>
+    );
+}
